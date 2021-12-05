51de195befb1a41a380b6716d36f0982d478fa2c
Jessie Broke <18238304+jessiebroke@users.noreply.github.com>
2021-12-04 20:43:34 -0500

    centered dad image

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
+
 export default function Home() {
   return (
     <div className="container">
@@ -12,9 +13,8 @@ export default function Home() {
 
       <main>
         <Header title="DAD DAO" />
-        <img src="/dad.png" alt="Dad image"/>
+        <img src="/dad.png" alt="Dad image" class="center"/>
         <p className="description">
-        
         Get started by joining the <a href="https://discord.gg/MBxGXSuGyp">Discord</a>
         </p>
       </main>
