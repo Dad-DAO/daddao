399e2fae628a3631b07beb051edcc2866098b22f
Jessie Broke <18238304+jessiebroke@users.noreply.github.com>
2021-12-04 23:20:11 -0500

    added santa hat icon

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
     <div className="container">
       <Head>
         <title>DAD DAO</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/santahat.ico" />
       </Head>
 
       <main>
