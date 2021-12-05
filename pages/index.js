4f039b6581f95a52e806b0059bab13d11702164a
Jessie Broke <18238304+jessiebroke@users.noreply.github.com>
2021-12-04 20:15:31 -0500

    modified main

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main>
         <Header title="DAD DAO" />
         <p className="description">
-        Get started by joining the Discord @ https://discord.gg/MBxGXSuGyp
+        Get started by joining the <a href="https://discord.gg/MBxGXSuGyp">Discord</a>
         </p>
       </main>
 
