f5ba39fecf6584cea756e661cb5777f53f240a7d
Jessie Broke <18238304+jessiebroke@users.noreply.github.com>
2022-03-18 03:01:00 -0400

    reverted changes

--- pages/index.js
+++ pages/index.js
@@ -14,43 +14,6 @@ export default function Home() {
         <h3>Community Learning and DApp Incubation</h3>
         <h5>Built on Algorand</h5>
         <img src="/dadgifsite.gif" alt="Dad NFTs" class="center"/>
-        <p className="description">
-        <p>Dad DAO Project Directory</p>
-        <p><br></br></p>
-        <p>Contacts</p>
-        <p>Broke#2219 (Core - Generalist)</p>
-        <p>Fergulati#5080 (Core - Business Dev)</p>
-        <p>mrtrosen#3835 (Core - Developer)</p>
-        <p>petty#7979 (Core - Generalist)</p>
-        <p>Jetblack#4158 &nbsp;(Core - Artist)</p>
-        <p>litty#5251 (Core - Marketing)</p>
-        <p>vlawmz#1337 (Freelance Developer)</p>
-        <p>Ken Berey#1598 (Freelance Marketing)</p>
-        <p>Frances#3341(Freelance Marketing)</p>
-        <p><br></br></p>
-        <p><br></br></p>
-        <p>Socials</p>
-        <p><a href="https://discord.gg/MBxGXSuGyp">https://discord.gg/MBxGXSuGyp</a> - Dad DAO Discord</p>
-        <p><a href="https://twitter.com/ThedaddaoDad">https://twitter.com/ThedaddaoDad</a> - Official Twitter</p>
-        <p><a href="https://www.reddit.com/r/DADDAO/">https://www.reddit.com/r/DADDAO/</a> - Official Reddit</p>
-        <p><br></br></p>
-        <p><br></br></p>
-        <p>Important Links for Dad DAO Project</p>
-        <p><a href="http://www.daddao.org">www.DadDAO.org</a> (Project Landing Page)</p>
-        <p><a href="http://www.github.com/Dad-DAO">www.github.com/Dad-DAO</a> - Repository with Projects (Public/Private Repos)</p>
-        <p><a href="https://hackmd.io/O9uamCHpQcKYjdqnvR5JbA">https://hackmd.io/O9uamCHpQcKYjdqnvR5JbA</a> - High-level Dad DAO for Social Media Posting</p>
-        <p><a href="https://hackmd.io/nmrqtiwoQa2qWTWVOkVESg">https://hackmd.io/nmrqtiwoQa2qWTWVOkVESg</a> - High-level Dad DAO for Social Media Posting</p>
-        <p><a href="https://hackmd.io/SAZfIUDOTEaVMYlft-9bVA">https://hackmd.io/SAZfIUDOTEaVMYlft-9bVA</a> - User Stories</p>
-        <p><a href="https://hackmd.io/7LKe7QU8Tyum8CjrVi8eyA">https://hackmd.io/7LKe7QU8Tyum8CjrVi8eyA</a> - Constitution</p>
-        <p><a href="https://hackmd.io/sjISxJKoSfCRx7x3EjPF3w?both">https://hackmd.io/sjISxJKoSfCRx7x3EjPF3w?both</a> - Core Team Meeting Minutes</p>
-        <p><a href="https://hackmd.io/u3GAKvhVQZOgyH3vZA4mfA">https://hackmd.io/u3GAKvhVQZOgyH3vZA4mfA</a> - Developer Information</p>
-        <p><a href="https://hackmd.io/fBXioRYZRByv1OeNWS4_CA">https://hackmd.io/fBXioRYZRByv1OeNWS4_CA</a> - Early Pre-Mint Guide</p>
-        <p><a href="https://hackmd.io/eWE9ut1YTamqTaa_rv1KqA">https://hackmd.io/eWE9ut1YTamqTaa_rv1KqA</a> - ARC-333 Proposal</p>
-        <p><a href="https://hackmd.io/92TGGR7jR5mDb9K-QZSj0g">https://hackmd.io/92TGGR7jR5mDb9K-QZSj0g</a> - Discord Bot Function Notes</p>
-        <p><a href="https://airtable.com/appxncC63rIXkv227">https://airtable.com/appxncC63rIXkv227</a> - Airtable Project Organization</p>
-        <p><a href="https://www.randgallery.com/algo-collection/?address=68344387&testnet">https://www.randgallery.com/algo-collection/?address=68344387&amp;testnet</a> - NFT Sample for Development Testing on Testnet</p>
-        Join the <a href="https://discord.gg/MBxGXSuGyp">Discord</a>
-        </p>
       </main>
       <Footer />
     </div>
