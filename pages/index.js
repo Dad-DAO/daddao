3e24598e093d31827ea1afb92e7611bc868789f1
Jessie Broke <18238304+jessiebroke@users.noreply.github.com>
2022-03-18 05:22:00 -0400

    adjusted text and image scaling to display properly

--- pages/index.js
+++ pages/index.js
@@ -10,19 +10,15 @@ export default function Home() {
         <link rel="icon" href="/santahat.ico" />
       </Head>
       <main>
-        <img src="/daddaologo.svg" alt="Dad DAO logo" class="imagelarge"/>
-        <br></br>
-        <h1 class="slogan">A Learning Community and DApp Incubator</h1>
+        <img src="/daddaologo.svg" alt="Dad DAO logo" class="logo"/>
+        <h3 class="slogan">A Learning Community and DApp Incubator</h3>
         <br></br>
         <a href="https://discord.gg/MBxGXSuGyp">Join the Discord</a>
         <br></br>
         <a href="https://docs.google.com/document/d/1f0jsi-Cu2qfEliEvdbHGi9t957cE1C4tAYgXhRqC1mw/edit">Project Directory</a>
         <br></br>
-        <img src="/dadgifsite.gif" alt="Dad NFTs" class="center"/>
-        <br></br>
-        <br></br>
-        <img src="/builtonalgo.svg" alt="Dad DAO Quote" class="center"/>
-        <br></br>
+        <img src="/dadgifsite.gif" alt="Dad NFTs" class="imagemedium"/>
+        <img src="/builtonalgo.svg" alt="Dad DAO Quote" class="imagesmall"/>
       </main>
       <Footer />
     </div>
